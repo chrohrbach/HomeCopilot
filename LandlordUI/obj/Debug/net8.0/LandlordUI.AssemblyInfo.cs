1ee3f7bbafb94766c175205ed123b74793aa4a76
Christian Rohrbach <crohrbach@hotmail.com>
2024-05-03 11:40:50 +0200

    Test

--- LandlordUI/obj/Debug/net8.0/LandlordUI.AssemblyInfo.cs
+++ LandlordUI/obj/Debug/net8.0/LandlordUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LandlordUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd34a24996eab74918c9ea764969428793492ca3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LandlordUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LandlordUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LenderUI/obj/Debug/net8.0/LenderUI.AssemblyInfo.cs
+++ LenderUI/obj/Debug/net8.0/LenderUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd34a24996eab74918c9ea764969428793492ca3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
