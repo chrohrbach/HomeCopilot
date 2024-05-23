c5317ffa10543895394474e2b7ffce105a12e6c7
Christian Rohrbach <crohrbach@teleia.ch>
2024-05-23 05:58:54 +0200

    bin

--- LenderUI/obj/Debug/net8.0/LenderUI.AssemblyInfo.cs
+++ LenderUI/obj/Debug/net8.0/LenderUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f8dc016c2702fdccf6ad74403320d105759d266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35e1dcd716ccfad5d430ae3e81dbc0cd21597c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LenderUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
